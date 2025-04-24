1897f019324652ed05c531c0f6fc7ccf5c0205a7
akshat-31415 <akshatcodez@gmail.com>
2025-04-24 19:25:04 +0530

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -19,6 +19,14 @@ func main() {
     	AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
 		}))
 
-	r.Run("0.0.0.0:8080")
+	//r.Run("0.0.0.0:8080")
+	port := os.Getenv("PORT")
+    	if port == "" {
+	        port = "10000" // Render uses 10000 by default if PORT is not set
+	    }
+	
+	fmt.Printf("Starting server on port %s\n", port)  // Log the port for debugging
+	r.Run("0.0.0.0:" + port) // Bind to all interfaces on the specified port
+
 
 }
